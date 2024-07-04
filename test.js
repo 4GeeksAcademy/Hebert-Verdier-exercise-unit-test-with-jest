d571cd6722f907732f0cdaede91809413279b17a
HSimonVS <hebert.verdier@estudiantes.utec.edu.uy>
2024-07-04 15:30:49 +0000

    Lineamientos de Codigo

--- test.js
+++ test.js
@@ -17,6 +17,7 @@ test("One euro should be 1.07 dollars", function() {
     // Uso la función como debe ser usada
     const dollars = fromEuroToDollar(3.5);
 
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
@@ -30,6 +31,7 @@ test("One dolar should be 146.26168224299064 yens", function() {
     // Uso la función como debe ser usada
     const yens = fromDollarToYen(3.5);
 
+    // Si 1 euro son 1.07 dólares, entonces 3.5 dolares debe ser (3.5 * 1.07) euros y ((3.5/1.07) * 156.5) yenes
     const expected = (3.5/1.07) * 156.5;
 
     // Hago mi comparación (la prueba)
@@ -43,6 +45,7 @@ test("One yen should be 0.0055591054313099035 pounds", function() {
     // Uso la función como debe ser usada
     const pounds = fromYenToPound(3.5);
 
+    // Si 1 euro son 156.5 yenes, entonces 3.5 yenes debe ser (3.5/156.5) euros y ((3.5/156.5) * 0.87) libras
     const expected = (3.5/156.5) * 0.87;
 
     // Hago mi comparación (la prueba)
